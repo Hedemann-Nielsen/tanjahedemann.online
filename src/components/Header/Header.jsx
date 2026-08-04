f37d973d4e4ddcdce2d0e71998b39a9b9b79246b
Hedemann-Nielsen <127857522+Hedemann-Nielsen@users.noreply.github.com>
2026-08-04 08:46:40 +0200

    Add Header and Hero components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header>
+      <div className="container">
+        <p>Tanja Hedemann</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+function Hero() {
+  return (
+    <section className="section">
+      <div className="container">
+        <p>UGC Creator & Content Specialist</p>
+
+        <h1>
+          Authentic content
+          <br />
+          for modern brands.
+        </h1>
+
+        <p>
+          I create warm, relatable and visually engaging content that helps
+          brands connect with their audience.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
